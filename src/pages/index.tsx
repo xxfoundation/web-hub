96c5284735d7ecef3bee7013fe43ba75323de6c8
Bernardo Cardoso <bernardo@xx.network>
2022-10-10 21:46:39 -0600

    Coming soon page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
-import { Container } from '@mui/material';
-import BasicTabs from '../components/BasicTabs';
+import { Container, Stack, Typography } from '@mui/material';
 
 const Home: FC = () => {
   return (
-    <Container maxWidth='lg' sx={{ py: 6 }}>
-      <BasicTabs />
+    <Container maxWidth='lg' sx={{ py: 12 }}>
+      <Stack direction='column' spacing='60px'>
+        <Typography variant='h3' align='center'>A unified website for xx network's tools, dapps, partnerships, etc...</Typography>
+        <Typography variant='h2' align='center'>Coming Soon!</Typography>
+      </Stack>
     </Container>
   );
 };
