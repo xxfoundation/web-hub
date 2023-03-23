b0ea6a5a56570e76b74561cf01b4be9a5814e6c8
Bernardo Cardoso <bernardo@xx.network>
2023-03-23 16:00:32 +0000

    Update subwallet logo and both wallets text

--- src/content/wallets.ts
+++ src/content/wallets.ts
@@ -6,7 +6,7 @@ import subWallet from '../assets/images/logos/subwallet.png';
 const wallets: Product[] = [
   {
     description:
-      'Nova Wallet is a non-custodial mobile wallet, where users have full control of their assets and their own private keys. It allows users to manage all substrate-based coins/tokens, enjoy convenient staking experiences, and interact with many blockchains without ever giving up your private keys or the passcode to your funds.',
+      'Nova Wallet is a non-custodial mobile wallet, where users have full control of their assets and their own private keys. It allows users to manage all substrate-based coins/tokens, enjoy convenient staking experiences, and interact with many blockchains.',
     url: 'https://novawallet.io/',
     tags: ['Mobile'],
     logo: novaWallet,
@@ -15,7 +15,7 @@ const wallets: Product[] = [
   },
   {
     description:
-      'SubWallet aspires to become a pioneering user-friendly Web3 Multiverse Gateway for the Polkadot and Kusama ecosystem. The main vision is a wallet in which users can track, send, receive, stake, contribute to crowdloans and manage multi-chain assets with utmost ease.',
+      'SubWallet is the comprehensive non-custodial wallet solution for Polkadot, Substrate & Ethereum ecosystems. We envision a Web3 multiverse gateway through which users can enjoy multichain services with utmost ease and absolute security.',
     url: 'https://subwallet.app/',
     tags: ['Browser Extension', 'Mobile'],
     logo: subWallet,
