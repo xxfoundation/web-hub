135aae7f6fa22126575ea8c44a204649799d47de
Bernardo Cardoso <bernardo@xx.network>
2023-06-10 02:58:14 +0000

    Add proxxy

--- src/content/dapps.ts
+++ src/content/dapps.ts
@@ -2,18 +2,9 @@ import type { Product } from '../types';
 
 import xxmessenger from '../assets/images/logos/xx_messenger_logotype_color.svg';
 import speakeasy from '../assets/images/logos/codename_speakeasy_logotype_black.png';
+import proxxy from '../assets/images/logos/proxxy.png';
 
 const dapps: Product[] = [
-  {
-    title: 'xx messenger',
-    description:
-      'The first and only messenger that truly protects communication between sender and receiver. All messages are not only end-to-end encrypted, but no metadata is ever – or can be – collected.',
-    url: 'https://elixxir.io/',
-    tags: ['Private Messenger'],
-    company: 'Elixxir',
-    companyUrl: 'https://elixxir.io',
-    logo: xxmessenger
-  },
   {
     title: 'Codename Speakeasy',
     description:
@@ -23,6 +14,26 @@ const dapps: Product[] = [
     company: 'Elixxir',
     companyUrl: 'https://elixxir.io',
     logo: speakeasy
+  },
+  {
+    title: 'Proxxy',
+    description:
+      'Protect your privacy when using web3. With Proxxy, any wallet can integrate better privacy, in order to avoid metadata being collected by RPC providers. Proxxy is currently available as a desktop application seamlessly integrated with MetaMask.',
+    url: 'https://proxxy.xx.network/',
+    tags: ['Privacy', 'Web3'],
+    company: 'xx foundation',
+    companyUrl: 'https://xx.network',
+    logo: proxxy
+  },
+  {
+    title: 'xx messenger',
+    description:
+      'The first and only messenger that truly protects communication between sender and receiver. All messages are not only end-to-end encrypted using quantum secure cryptography, but no metadata is ever – or can be – collected.',
+    url: 'https://elixxir.io/',
+    tags: ['Private Messenger'],
+    company: 'Elixxir',
+    companyUrl: 'https://elixxir.io',
+    logo: xxmessenger
   }
 ];
 
