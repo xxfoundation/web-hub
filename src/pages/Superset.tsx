b4ad3c62402bb7b0069082abb97e09cc4a86e771
Keith Millette <keith@xx.network>
2025-07-28 11:09:21 +0900

    Add box for text.

--- src/pages/Superset.tsx
+++ src/pages/Superset.tsx
@@ -1,8 +1,9 @@
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 
 function Superset() {
   return (
     <Container maxWidth='lg' sx={{ py: 6 }}>
+      <Box>⚠️ A space to add some caveats and notes</Box>
       <iframe
             src={'https://f341f250-e8a6-4013-93c0-b96133543814.bridges.rollbridge.app/?widget=true&fromChainId=1&toChainId=56&tokenAddress=0x171120219d3223E008558654ec3254A0F206edb2'}
             width='100%'
